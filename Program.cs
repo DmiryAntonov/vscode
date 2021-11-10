60cd2f321344af1702c95c684f5f63d9daab1535
dmitrytestsqa <antonovdmitry90@gmail.com>
2021-11-10 17:05:35 +0200

    git add

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello,456546764ld, World, World!");
-        Console.WriteLine("Hello the second line");
-        Console.WriteLine("H5674567rld!");
-        Console.WriteLine("Hellow, W5674567 World!");
-        Console.WriteLine("Hello, 54675475rtyrety4!");
-        Console.WriteLine("54754fgdhd7547World!");
+        Console.WriteLine("Hellgfdhdgfh");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
 
 List<string> ds = new List<string>();
 
