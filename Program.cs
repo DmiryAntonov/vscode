ccc69dbc269e4dc34c1c94d86b4e5e0eb2806533
dmitrytestsqa <antonovdmitry90@gmail.com>
2021-11-10 16:33:38 +0200

    new commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
         Console.WriteLine("Hello,456546764ld, World, World!");
         Console.WriteLine("Hello the second line");
         Console.WriteLine("H5674567rld!");
-        Console.WriteLine("Hello, W5674567 World!");
+        Console.WriteLine("Hellow, W5674567 World!");
         Console.WriteLine("Hello, 546754754!");
         Console.WriteLine("547547547World!");
 
