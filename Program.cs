31085faf18f958b7f17d4640bfdfe0d85dd89840
dmitrytestsqa <antonovdmitry90@gmail.com>
2021-11-10 17:03:17 +0200

    edited lines

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ class Program
         Console.WriteLine("Hello the second line");
         Console.WriteLine("H5674567rld!");
         Console.WriteLine("Hellow, W5674567 World!");
-        Console.WriteLine("Hello, 546754754!");
-        Console.WriteLine("547547547World!");
+        Console.WriteLine("Hello, 54675475rtyrety4!");
+        Console.WriteLine("54754fgdhd7547World!");
 
 List<string> ds = new List<string>();
 
