4e2c42a0b5ff01a4f6a25af2b9ccaf5b602c5603
dmitrytestsqa <antonovdmitry90@gmail.com>
2021-11-10 16:11:31 +0200

    new lines

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World, World, World!");
+        Console.WriteLine("Hello,456546764ld, World, World!");
         Console.WriteLine("Hello the second line");
+        Console.WriteLine("H5674567rld!");
+        Console.WriteLine("Hello, W5674567 World!");
+        Console.WriteLine("Hello, 546754754!");
+        Console.WriteLine("547547547World!");
     }
 }
