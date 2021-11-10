49627e933b4849536df6d7734941ef64554f2384
dmitrytestsqa <antonovdmitry90@gmail.com>
2021-11-10 16:04:36 +0200

    hello from the second line in the feature branch

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World, World, World!");
-        Console.WriteLine("Hello first line");
+        Console.WriteLine("Hello the second line");
     }
 }
