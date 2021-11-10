4447deab75ac554fcae9cceb3b9604ae125bde2f
DmiryAntonov <46794083+DmiryAntonov@users.noreply.github.com>
2021-11-10 17:04:51 +0200

    new changes

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello,456546764ld, World, World!");
-        Console.WriteLine("Hello the second line");
-        Console.WriteLine("H5674567rld!");
-        Console.WriteLine("Hellow, W5674567 World!");
-        Console.WriteLine("Hello, 54675475rtyrety4!");
+        Console.WriteLine("");
+        Console.WriteLine("H53453254d!");
+        Console.WriteLine("Hellow, "gfjh");
+        Console.WriteLine("Hegfhjgfhjgfjo, 54675475rtyrety4!");
         Console.WriteLine("54754fgdhd7547World!");
 
 List<string> ds = new List<string>();
